662d4f19dd3bad22e6b239904889675f4eee5227
Tommy Jones <jones.thos.w@gmail.com>
2015-12-13 22:58:32 -0500

    MOAR pre-cran updates

--- R/NgramTokenizer.R
+++ R/NgramTokenizer.R
@@ -1,5 +1,18 @@
-# Get n-grams when creating a document term matrix
-# This function is undocumented/internal to the package and not designed to be
+#' @title Get n-grams when creating a document term matrix
+#' @description This function is a helper function for \code{textmineR}. It is
+#' not designed for users. It aids in creating n-grams in DTM construction
+#' @param min Smallest n-gram desired
+#' @param max Largest n-gram desired
+#' @return Returns a function
+#' @export
+#' @examples
+#' \dontrun{
+#' my_tokenizer <- NgramTokenizer(min=1, max=3)
+#' 
+#' dtm <- tm::DocumentTermMatrix(corp, control=list(tokenize=my_tokenizer))
+#' 
+#' dtm <- MakeSparseDTM(dtm)
+#' }
 # called by users.
 NgramTokenizer <- function(min, max) {
   
---
+++ R/RecursiveRbind.R
@@ -0,0 +1,35 @@
+#' @title Recursively call rBind from the Matrix package.
+#' @description This is used for combining a list of sparce matrices into a large Matrix.
+#' This is a worker function for \code{textmineR} and generally not made to be
+#' used by users. However, the function is provided for custom functions. 
+#' @param matrix_list A list, each element containing a matrix of class \code{dgCMatrix}
+#' @return
+#' Returns a matrix of class \code{dgCMatrix}. 
+#' @examples
+#' a_matrix <- Matrix::Matrix(0, nrow=10, ncol=10)
+#' 
+#' a_list <- list(a_matrix, a_matrix, a_matrix, a_matrix, a_matrix)
+#' 
+#' result <- RecursiveRbind(a_list)
+#' 
+#' @export 
+
+RecursiveRbind <- function(matrix_list){
+  
+  if(length(matrix_list) <= 100){
+    
+    return(do.call(Matrix::rBind, matrix_list))
+    
+  }else{
+    batches <- seq(1, length(matrix_list), by = 100)
+    
+    matrix_list <- lapply(batches, function(x){
+      do.call(Matrix::rBind, matrix_list[ x:min(x + 99, length(matrix_list))])
+    })
+    
+    RecursiveRbind(matrix_list=matrix_list)
+  }
+  
+}
+
+
