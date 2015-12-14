35be8a917ea502a0348557e188b76cefe99afc40
Tommy Jones <jones.thos.w@gmail.com>
2015-12-13 20:23:41 -0500

    Add documentation to MakeSparseDtm

--- R/MakeSparseDTM.R
+++ R/MakeSparseDTM.R
@@ -1,7 +1,21 @@
-# Convert a sparse simple triplet document term matrix (tm package) to a sparse 
-# Matrix object (Matrix package).
-# This function is undocumented/internal to the package and not designed to be
-# called by users.
+#' Convert a sparse simple triplet document term matrix to a sparse Matrix
+#' 
+#' @description This function takes a \code{DocumentTermMatrix} from the 
+#' \code{tm} package, and converts it to a \code{CSparse} matrix from the 
+#' \code{Matrix} package. Generally, it is designed to be an internal function
+#' to \code{textmineR}. However, it is available in case users want to use the 
+#' \code{tm} package directly to make a DTM.
+#' @param dtm A \code{DocumentTermMatrix} object from the \code{tm} package
+#' @return
+#' Returns an object of class \code{dgCMatrix}, a sparse matrix, whose columns
+#' correspond to terms and rows correspond to documents.
+#' @export
+#' @examples
+#' \dontrun{
+#' my_dtm <- tm::DocumentTermMatrix(my_corpus_object)
+#' MakeSparseDtm(dtm = my_dtm)
+#' }
+
 
 MakeSparseDTM <- function(dtm){
   # dtm is a simple triplet matrix
