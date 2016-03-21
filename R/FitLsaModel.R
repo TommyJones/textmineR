c43f44db8a7bec25e65c462089b2bb20d5bab6dc
Tommy Jones <jones.thos.w@gmail.com>
2016-03-20 20:45:24 -0400

    Add functionality for LSA

---
+++ R/FitLsaModel.R
@@ -0,0 +1,69 @@
+#' Fit a topic model using Latent Semantic Analysis
+#' @description A wrapper for \code{irlba::irlba} that returns 
+#' a nicely-formatted latent semantic analysis topic model.
+#' @param dtm A document term matrix of class \code{Matrix::dgCMatrix}
+#' @param k Number of topics
+#' @param return_all Should all objects returned from \code{irlba::irlba} be
+#'        returned here? Defaults to \code{FALSE}
+#' @param ... Other arguments to pass to \code{irlba::irlba}. 
+#' @return Returns a list with a minimum of three objects: \code{phi},  
+#' \code{theta}, and \code{sv}. The rows of \code{phi} index topics and the 
+#' columns index tokens. The rows of \code{theta} index documents and the 
+#' columns index topics. \code{sv} is a vector of singular values.
+#' @details Latent semantic analysis, LSA, uses single value decomposition to
+#' factor the document term matrix. In many LSA applications, TF-IDF weights are
+#' applied to the DTM before model fitting. However, this is not strictly 
+#' necessary. 
+#' @examples
+#' # Load a pre-formatted dtm 
+#' data(acq2) 
+#' 
+#' # Convert raw word counts to TF-IDF frequency weights
+#' idf <- log(nrow(dtm) / Matrix::colSums(dtm > 0))
+#' 
+#' dtm_tfidf <- Matrix::t(dtm) * idf
+#' 
+#' dtm_tfidf <- Matrix::t(dtm_tfidf)
+#' 
+#' # Fit an LSA model
+#' model <- FitLsaModel(dtm = dtm_tfidf, k = 5)
+#' 
+#' str(model)
+#' 
+#' # Fit a model, centering each document, by passing the center argument to irlba
+#' model <- FitLsaModel(dtm = dtm, k = 5, center = Matrix::rowMeans(dtm))
+#' 
+#' str(model)
+#' @export
+FitLsaModel <- function(dtm, k, return_all = FALSE, ...){
+  
+  # Fit LSA using single value decomposition on sparse matrices from 
+  # irlba library
+  lsa <- irlba::irlba(dtm, nv=10)
+  
+  # Rename/transform objects so they conform to the convention in textmineR
+  names(lsa)[ names(lsa) == "v" ] <- "phi"
+  lsa$phi <- t(lsa$phi)
+  colnames(lsa$phi) <- colnames(dtm)
+  rownames(lsa$phi) <- paste("t", 1:nrow(lsa$phi), sep="_")
+  
+  names(lsa)[ names(lsa) == "u" ] <- "theta"
+  rownames(lsa$theta) <- rownames(dtm)
+  colnames(lsa$theta) <- rownames(lsa$phi)
+  
+  names(lsa)[ names(lsa) == "d" ] <- "sv"
+  names(lsa$sv) <- names(lsa$theta)
+  
+  if(! return_all ){
+    lsa$iter <- NULL
+    lsa$mprod <- NULL
+  }
+  
+  return(lsa)
+  
+}
+
+
+
+
+
