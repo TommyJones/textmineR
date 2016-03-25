10ace20150d2351de0a855c44cf4f857c89f4c93
Tommy Jones <jones.thos.w@gmail.com>
2016-03-25 16:04:13 -0400

    adding a skeleton for upcoming function

---
+++ extra_functions/fit_topic_model.R
@@ -0,0 +1,25 @@
+
+fit_topic_model <- function(x, k = NULL, type = c("lda", "lsa", "stm", "pca", "ca", "glove",
+                                        "ctm", "cluster"), 
+                            control = list(...)){
+  
+  # Latent Dirichlet Allocation
+  # (em vs gibbs)
+  
+  # Latent Semantic Analysis
+  
+  # Structural Topic Model
+  
+  # Principle Component Analsis
+  
+  # Correspondence Analysis
+  
+  # GloVe
+  
+  # Correlated Topic Model
+  
+  
+}
+
+
+
