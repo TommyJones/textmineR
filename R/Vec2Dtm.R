af337b8f1e4e155837af0cc05cfb0a48aa7ff08f
Tommy Jones <jones.thos.w@gmail.com>
2016-03-31 20:47:20 -0400

    text2vec version of vec2dtm

--- R/Vec2Dtm.R
+++ R/Vec2Dtm.R
@@ -1,111 +1,121 @@
 #' @title Convert a character vector to a document term matrix of class Matrix.
 #' @description This is the main document term matrix creating function for \code{textmineR}.
-#' In most cases, all you need to do is import documents as a character vector in R and then run this function
-#' to get a document term matrix that is compatible with the rest of \code{textmineR}'s functionality.
+#' In most cases, all you need to do is import documents as a character vector in R and then 
+#' run this function to get a document term matrix that is compatible with the 
+#' rest of \code{textmineR}'s functionality and many other libraries. \code{vec2dtm}
+#' is built on top of the excellent \code{link[text2vec]{text2vec}}library.
 #' 
-#' @param vec A character vector of documents. Punctuation etc. is allowed. names(vec) should be names of your documents.
+#' @param doc_vec A character vector of documents. 
 #' @param docnames A vector of names for your documents. Defaults to 
 #'        \code{names(doc_vec)}. If NULL, then docnames is set to be 
 #'        \code{1:length(doc_vec)}.
-#' @param min.n.gram The minimum size of n for creating n-grams. (Defaults to 1)
-#' @param max.n.gram The maximum size of n for creating n-grams. (Defaults to 1. Numbers greater than 3 are discouraged due to risk of overfitting.)
-#' @param remove.stopwords Do you want to remove standard stopwords from your documents? (Defaults to TRUE)
-#' @param custom.stopwords If not NULL (the default) a character vector of additional stopwords to remove from your corpus. Note: it is generally faster to
-#' remove additional stopwords by subsetting the columns of a DTM post-hoc. This is to be used when you want these words removed before creatign the DTM.
-#' @param lower Do you want all words coerced to lower case? (Defaults to TRUE)
-#' @param remove.punctuation Do you want to convert all punctuation to spaces? For example, "big-lipped fish" goes to "big lipped fish" (Defaults to TRUE)
-#' @param remove.numbers Do you want to convert all numbers to spaces? For example, "3rd grade teachers" goes to " rd grade teachers" (Defaults to TRUE)
-#' @param stem.document Do you want to stem the words in your document? (Defaults to FALSE)
-#' @param ... Additional arguments to be passed to \code{textmineR::TmParallelApply}
-#' @note
-#' This function relies heavily on the \code{tm} and \code{RWeka} packages. N-grams are derived using the \code{RWeka} package. 
-#' There is a confilct between \code{RWeka} and \code{parallel} (used for multithread processing on unix-like systems).
-#' Consequently, using n-grams (n > 1) causes construction of the DTM to be considerably slower on unix systems. 
-#' Speed is unaffected on Windows machines because parallelization is not supported for DTM construction.
-#'
-#' @export
+#' @param min_ngram The minimum size of n for creating n-grams. Defaults to 1.
+#' @param max_ngram The maximum size of n for creating n-grams. Defaults to 1. 
+#'        Numbers greater than 3 are discouraged due to risk of overfitting.
+#' @param remove_stopwords Do you want to remove standard stopwords from your documents? 
+#'        Defaults to \code{TRUE}.
+#' @param custom_stopwords If not \code{NULL} (the default) a character vector 
+#'        of stopwords to remove from your corpus. 
+#' @param lower Do you want all words coerced to lower case? Defaults to \code{TRUE}
+#' @param remove_punctuation Do you want to convert all non-alpha numeric 
+#'        characters to spaces? Defaults to \code{TRUE}
+#' @param remove_numbers Do you want to convert all numbers to spaces? Defaults 
+#'        to \code{TRUE}
+#' @param stem_documents Do you want to stem the words in your document using 
+#'        Porter's word stemmer? Defaults to \code{FALSE}
+#' @param ... Other arguments to be passed to \code{textmineR::TmParallelApply}.
 #' @examples
-#' \dontrun{
 #' data(nih_sample)
 #' 
-#' dtm <- Vec2Dtm(vec = nih_sample$ABSTRACT_TEXT
+#' 
+#' dtm <- vec2dtm(doc_vec = nih_sample$ABSTRACT_TEXT,
 #'                docnames = nih_sample$APPLICATION_ID, 
-#'                min.n.gram = 1, max.n.gram = 2)
+#'                min_ngram = 1, max_ngram = 2)
 #' 
 #' dim(dtm)
 #' 
 #' head(colnames(dtm))
 #' 
 #' head(rownames(dtm))
-#' }
-Vec2Dtm <- function(vec, docnames = names(vec), min.n.gram=1, max.n.gram=1, 
-                    remove.stopwords=TRUE, custom.stopwords=NULL, lower=TRUE, 
-                    remove.punctuation=TRUE, remove.numbers=TRUE, stem.document=FALSE){
-  # for now, it is strongly advised to accept the defaults for lower, remove.punctuation, and remove.numbers
-  # Other functions are built assuming that the column headings of a dtm contain only letters and underscores "_"
+#' @export
+vec2dtm <- function(doc_vec, docnames = names(doc_vec), min_ngram = 1, max_ngram = 1, 
+                    remove_stopwords = TRUE, custom_stopwords = NULL, 
+                    lower = TRUE, remove_punctuation = TRUE, remove_numbers = TRUE,
+                    stem_documents = FALSE, ...){
   
+  ### Pre-process the documents ------------------------------------------------
   if(is.null(docnames)){
-    warning("No document names detected. Assigning 1:length(vec) as names.")
-    docnames <- 1:length(vec)
-  }  
+    warning("No document names detected. Assigning 1:length(doc_vec) as names.")
+    docnames <- 1:length(doc_vec)
+  }
   
-  options(mc.cores = parallel::detectCores())
-  
-  if(remove.stopwords){
+  if (remove_stopwords) {
     stopwords <- unique(c(tm::stopwords("english"), tm::stopwords("SMART")))
-  }else{
+  }
+  else {
     stopwords <- c()
   }
   
-  if(is.null(names(vec))){
-    docnames <- 1:length(vec)
-  }else{
-    docnames <- names(vec)
-  }
+  if (!is.null(custom_stopwords)){
+    stopwords <- c(stopwords, custom.stopwords)
+  } 
   
-  if( ! is.null(custom.stopwords) ) stopwords <- c(stopwords, custom.stopwords)
-  
-  if( lower ) {
-    vec <- tolower(vec)
+  if (lower) {
+    doc_vec <- tolower(doc_vec)
     stopwords <- tolower(stopwords)
   }
   
-  if( remove.punctuation ){ 
-    vec <- gsub("[^a-zA-Z0-9]", " ", vec)
-    stopwords <- gsub("[^a-zA-Z0-9]", " ", stopwords)
-    stopwords <- unique(unlist(strsplit(stopwords, split="\\s+")))
+  if (remove_punctuation) {
+    doc_vec <- stringr::str_replace_all(doc_vec, "[^a-zA-Z0-9]", " ")
+    stopwords <- stringr::str_replace_all(stopwords, "[^a-zA-Z0-9]", " ")
+    stopwords <- unique(unlist(stringr::str_split(string = stopwords, 
+                                                  pattern = "\\s+")))
   }
   
-  if( remove.numbers ){ 
-    vec <- gsub("[0-9]", " ", vec)
+  if (remove_numbers) {
+    doc_vec <- stringr::str_replace_all(doc_vec, "[0-9]", " ")
   }
   
-  vec <- gsub("\\s+", " ", vec) # remove extra spaces
+  doc_vec <- stringr::str_replace_all(doc_vec, "\\s+", " ")
   
-  corp <- tm::Corpus(tm::VectorSource(vec))
+  ### Create iterators, vocabulary, other objects for dtm construction ---------
   
+  # tokenize & construct vocabulary
+  tokens <- text2vec::word_tokenizer(string = doc_vec)
   
-  if( remove.stopwords | ! is.null(custom.stopwords)){
-    corp <- tm::tm_map(x=corp, tm::removeWords, stopwords)
+  if(remove_stopwords | ! is.null(custom_stopwords)){
+    tokens <- textmineR::TmParallelApply(X = tokens, FUN = function(x){
+      setdiff(x, stopwords)
+    }, export = "stopwords", ...)
   }
   
-  if( stem.document ){
-    corp <- tm::tm_map(x=corp, tm::stemDocument)
+  
+  if(stem_documents){
+    tokens <- textmineR::TmParallelApply(X = tokens, FUN = function(x){
+      SnowballC::wordStem(x, "porter")
+    }, ...)
   }
   
-  if(max.n.gram == 1){
-    dtm <- tm::DocumentTermMatrix(corp)
-  }else{
-    options(mc.cores=1)
-    dtm <- tm::DocumentTermMatrix(corp, control=list(
-      tokenize=NgramTokenizer(min=min.n.gram, max=max.n.gram)))
-  }
+  it <- text2vec::itoken(tokens)
   
-  dtm <- MakeSparseDTM(dtm=dtm)
+  vocabulary <- text2vec::create_vocabulary(itoken_src = it, 
+                                            ngram = c(ngram_min = as.integer(min_ngram),
+                                                      ngram_max = as.integer(max_ngram)))
   
-  colnames(dtm) <- gsub(" ", "_", colnames(dtm))
+  
+  vectorizer <- text2vec::vocab_vectorizer(vocabulary = vocabulary)
+  
+  ### Get the dtm, make sure it has correct dimnames, and return ---------------
+  it <- text2vec::itoken(tokens)
+  
+  corpus <- text2vec::create_corpus(it, vectorizer)
+  
+  dtm <- text2vec::get_dtm(corpus, type = "dgCMatrix")
   
   rownames(dtm) <- docnames
   
   return(dtm)
 }
+
+
+
