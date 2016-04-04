07e1207d5ed2e6c6ed65f608ccfeec5d1dd73bb2
Tommy Jones <jones.thos.w@gmail.com>
2016-04-04 19:01:21 -0400

    Adding CalcPhiPrime as an update to GetPhiPrime. Deprecating GetPhiPrime.

--- R/GetPhiPrime.R
+++ R/GetPhiPrime.R
@@ -20,33 +20,12 @@
 #'                          theta = nih_sample_topic_model$theta)
 #' 
 GetPhiPrime <- function(phi, theta){
+  .Deprecated(new = "CalcPhiPrime", package = "textmineR",
+              msg = "GetPhiPrime is deprecated and will be removed in textmineR v2.5.0
+              Use 'CalcPhiPrime' instead.",
+              old = "GetPhiPrime")
 
-    # set up constants
-    D <- nrow(theta)
-    K <- ncol(theta)
-    V <- ncol(phi)
-    
-    # probability of each document (assumed to be equiprobable)
-    p.d <- rep(1/nrow(theta), nrow(theta))
-    
-    # get the probability of each topic
-    p.t <- p.d %*% theta
-    
-    # get the probability of each word from the model    
-    p.w <- p.t %*% phi
-    
-
-    
-    # get our result
-    phi.prime <- matrix(0, ncol=ncol(p.t), nrow=ncol(p.t))
-    diag(phi.prime) <- p.t
-    
-    phi.prime <- phi.prime %*% phi
-    
-    phi.prime <- t(apply(phi.prime, 1, function(x) x / p.w))
-    
-    rownames(phi.prime) <- rownames(phi)
-    colnames(phi.prime) <- colnames(phi)
+  phi.prime <- CalcPhiPrime(phi = phi, theta = theta)
     
     return(phi.prime)
 }
