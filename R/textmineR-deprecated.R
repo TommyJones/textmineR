4c8c7d198ede69aface51de839409c84c192f9bb
Tommy Jones <jones.thos.w@gmail.com>
2018-04-28 21:57:44 -0400

    Adding a deprecated help page

---
+++ R/textmineR-deprecated.R
@@ -0,0 +1,11 @@
+## textmineR-deprecated.r
+#' @title Deprecated functions in package \pkg{textmineR}.
+#' @description The functions listed below are deprecated and will be defunct in
+#'   the near future. When possible, alternative functions with similar
+#'   functionality are also mentioned.
+#' @name textmineR-deprecated
+#' @keywords internal
+#' @details 
+#' Below is a list of deprecated functions:
+#' RecursiveRbind
+NULL
