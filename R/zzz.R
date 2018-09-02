edab412683f616a15deb9a6c08be85260cbab415
Tommy Jones <jones.thos.w@gmail.com>
2018-09-01 22:33:00 -0400

    moar spacing fix

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   packageStartupMessage("textmineR v3.0 is coming with major changes that WILL break things!
    Please see https://github.com/TommyJones/textmineR/tree/3.0 for the 
    development version. Expected release is October or November of 2018.
-   Please submit questions or requests to 	https://github.com/TommyJones/textmineR/issues")
+   Please submit questions or requests to https://github.com/TommyJones/textmineR/issues")
 }
 
 
