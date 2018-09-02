ec2e8e30483b0862cde99348110abbe77893398b
Tommy Jones <jones.thos.w@gmail.com>
2018-09-01 22:25:24 -0400

    ok I guess I only needed .onAttach(). Programming is hard.

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-  
-  packageStartupMessage("textmineR v3.0 is coming with major changes that WILL break things!
-                        Please see https://github.com/TommyJones/textmineR/tree/3.0 for the 
-                        development version. Expected release is October or November of 2018.
-                        Please submit questions or requests to 	https://github.com/TommyJones/textmineR/issues")
-  
-}
-
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("textmineR v3.0 is coming with major changes that WILL break things!
                         Please see https://github.com/TommyJones/textmineR/tree/3.0 for the 
