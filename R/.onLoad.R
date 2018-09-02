57120d146f05970a0a92525aef87a0d1c29ecfad
Tommy Jones <jones.thos.w@gmail.com>
2018-09-01 22:08:17 -0400

    Display a message at startup of textmineR

---
+++ R/.onLoad.R
@@ -0,0 +1,8 @@
+.onLoad <- function() {
+  
+  packageStartupMessage("textmineR v3.0 is coming with major changes that WILL break things!
+                        Please see https://github.com/TommyJones/textmineR/tree/3.0 for the 
+                        development version. Expected release is October or November of 2018.
+                        Please submit questions or requests to 	https://github.com/TommyJones/textmineR/issues")
+  
+}
