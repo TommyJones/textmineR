9accdab5c1588ebfafde34b3ec80d068ab57139a
Tommy Jones <jones.thos.w@gmail.com>
2018-10-14 22:22:58 -0400

    using roxygen2 to generate namespace. Also adding help file for textmineR itself

---
+++ R/textmineR.R
@@ -0,0 +1,21 @@
+#' textmineR
+#'
+#' Functions for Text Mining and Topic Modeling
+#'
+#' An aid for text mining in R, with a syntax that
+#' should be familiar to experienced R users. Provides a wrapper for several 
+#' topic models that take similarly-formatted input and give similarly-formatted
+#' output. Has additional functionality for analyzing and diagnostics for
+#' topic models.
+#'
+#' @name textmineR
+#' @docType package
+NULL
+
+#' @import Matrix
+#' @import Rcpp
+#' @import RcppProgress
+#' @importFrom methods as
+#' @importFrom futile.logger flog.debug flog.info flog.warn flog.error
+#' @useDynLib "textmineR", .registration=TRUE
+NULL
