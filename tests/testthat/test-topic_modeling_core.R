0f2ddcae9ccfaf822f411611cb32c81694574fdd
Tommy Jones <jones.thos.w@gmail.com>
2018-11-26 20:48:21 -0800

    adding preliminary tests for update.lda_topic_model

--- tests/testthat/test-topic_modeling_core.R
+++ tests/testthat/test-topic_modeling_core.R
@@ -343,7 +343,59 @@ test_that("predict.lda_topic_model",{
   
 })
 
-
+### update.lda_topic_model ----
+test_that("update.lda_topic_model",{
+  
+  # TODO: (1) add expect_true statements (2) account for different vocab in d1 and d2
+  
+  # load a document term matrix
+  d1 <- nih_sample_dtm[1:50,]
+  
+  d2 <- nih_sample_dtm[51:100,]
+  
+  # fit a model
+  m <- FitLdaModel(d1, k = 10, 
+                   iterations = 200, burnin = 175,
+                   optimize_alpha = TRUE, 
+                   calc_likelihood = FALSE,
+                   calc_coherence = TRUE,
+                   calc_r2 = FALSE)
+  
+  # update an existing model by adding documents
+  m2 <- update(object = m,
+               dtm = rbind(d1, d2),
+               iterations = 200,
+               burnin = 175)
+  
+  expect_true(sum(dim(m$phi) == dim(m2$phi)) == 2)
+  
+  # use an old model as a prior for a new model
+  m3 <- update(object = m,
+               dtm = d2, # new documents only
+               iterations = 200,
+               burnin = 175)
+  
+  expect_true(sum(dim(m$phi) == dim(m3$phi)) == 2)
+  
+  
+  # add topics while updating a model by adding documents
+  m4 <- update(object = m,
+               dtm = rbind(d1, d2),
+               additional_k = 3,
+               iterations = 200,
+               burnin = 175)
+  
+  # expect_true(ncol(m$))
+  
+  
+  # add topics to an existing model
+  m5 <- update(object = m,
+               dtm = d1, # this is the old data
+               additional_k = 3,
+               iterations = 200,
+               burnin = 175)
+  
+})
 
 
 
