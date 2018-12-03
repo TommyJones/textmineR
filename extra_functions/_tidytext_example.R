3da4abc51479f3568129f04a7d8d58ad146668a7
Tommy Jones <jones.thos.w@gmail.com>
2018-12-02 21:21:08 -0500

    updates based on BJ's suggestions and a little elbow grease of my own.

--- extra_functions/_tidytext_example.R
+++ extra_functions/_tidytext_example.R
@@ -4,26 +4,30 @@
 
 library(tidytext)
 library(textmineR)
+library(dplyr)
+library(tidyr)
 
 # load documents in a data frame
-docs <- textmineR::nih_sample
+docs <- textmineR::nih_sample 
 
 # tokenize using tidytext's unnest_tokens
-# using arguments similar to what we'd get from CreateDtm
-tidy_docs <- unnest_tokens(tbl = docs[,c("APPLICATION_ID", "ABSTRACT_TEXT")], 
-                           output = word, 
-                           input = ABSTRACT_TEXT,
-                           stopwords = c(stopwords::stopwords("en"), 
-                                         stopwords::stopwords(source = "smart")),
-                           token = "ngrams",
-                           n_min = 1, n = 2) 
+tidy_docs <- docs %>% 
+  select(APPLICATION_ID, ABSTRACT_TEXT) %>% 
+  unnest_tokens(output = word, 
+                input = ABSTRACT_TEXT,
+                stopwords = c(stopwords::stopwords("en"), 
+                              stopwords::stopwords(source = "smart")),
+                token = "ngrams",
+                n_min = 1, n = 2) %>% 
+  count(APPLICATION_ID, word) %>% 
+  filter(n>1) #Filtering for words/bigrams per document, rather than per corpus
+
+tidy_docs <- tidy_docs %>% # filter words that are just numbers
+  filter(! stringr::str_detect(tidy_docs$word, "^[0-9]+$"))
 
 # turn a tidy tbl into a sparse dgCMatrix for use in textmineR
-d <- cast_sparse(data = cbind(tidy_docs, count = 1), 
-                 row = APPLICATION_ID, column = word, value = count)
-
-# remove the lowest-frequency words before modeling
-d <- d[,colSums(d) > 1]
+d <- tidy_docs %>% 
+  cast_sparse(APPLICATION_ID, word, n)
 
 
 # create a topic model
@@ -38,28 +42,14 @@ m <- FitLdaModel(dtm = d,
 
 # below is equivalent to tidy_beta <- tidy(x = m, matrix = "beta")
 tidy_beta <- data.frame(topic = as.integer(stringr::str_replace_all(rownames(m$phi), "t_", "")), 
-                m$phi, 
-                stringsAsFactors = FALSE) %>%
-  reshape2::melt(id.vars = "topic", 
-             variable.name = "term",
-             value.name = "beta") %>%
+                        m$phi, 
+                        stringsAsFactors = FALSE) %>%
+  gather(term, beta, -topic) %>% 
   tibble::as.tibble()
-
-tidy_beta$term <- as.character(tidy_beta$term)
-
-tidy_beta
 
 # below is equivalent to tidy_gamma <- tidy(x = m, matrix = "gamma")
 tidy_gamma <- data.frame(document = rownames(m$theta),
                          m$theta,
                          stringsAsFactors = FALSE) %>%
-  reshape2::melt(id.vars = "document", 
-                 variable.name = "topic",
-                 value.name = "gamma") %>%
+  gather(topic, gamma, -document) %>%
   tibble::as.tibble()
-
-tidy_gamma$topic <- as.character(tidy_gamma$topic) %>%
-  stringr::str_replace_all("t_", "") %>%
-  as.integer()
-
-tidy_gamma
