39c39ea0efbc3aed678cd6d7868867e876f4a78e
Matt-Int <mattias1126@protonmail.com>
2021-06-28 22:59:17 +0100

    Updated CalcTopicModelR2 documentation to reflect new internals
    Also added some new lines to keep to 80 characters.

--- R/evaluation_metrics.R
+++ R/evaluation_metrics.R
@@ -216,21 +216,23 @@ CalcProbCoherence<- function(phi, dtm, M = 5){
 
 #' Calculate the R-squared of a topic model.
 #' @description Function to calculate R-squared for a topic model. 
-#' This uses a geometric interpretation of R-squared as the proportion of total distance 
-#' each document is from the center of all the documents that is explained by the model. 
+#' This is a wrapper for \code{mvrsquared::calc_rsquared} with additional
+#' checks in place to check for the inputs.
 #' @param dtm A documents by terms dimensional document term matrix of class
 #' \code{dgCMatrix} or of class \code{matrix}. 
-#' @param phi A topics by terms dimensional matrix where each entry is p(term_i |topic_j)
-#' @param theta A documents by topics dimensional matrix where each entry is p(topic_j|document_d)
-#' @param ... Other arguments to be passed to \code{\link[textmineR]{TmParallelApply}}. See note, below.
+#' @param phi A topics by terms dimensional matrix where each entry is
+#' p(term_i |topic_j)
+#' @param theta A documents by topics dimensional matrix where each entry
+#' is p(topic_j|document_d)
+#' @param ... Other arguments to be passed to
+#' \code{\link[mvrsquared]{calc_rsquared}}. See note, below.
 #' @return
-#' Returns an object of class \code{numeric} representing the proportion of variability
-#' in the data that is explained by the topic model.
+#' Returns an object of class \code{numeric} representing the proportion
+#' of variability in the data that is explained by the topic model.
 #' @note
-#' This function performs parallel computation if \code{dtm} has more than 3,000
-#' rows. The default is to use all available cores according to \code{\link[parallel]{detectCores}}.
-#' However, this can be modified by passing the \code{cpus} argument when calling
-#' this function.
+#' This function performs parallel computation if \code{threads} is set to
+#' more than 1. The default is to use 1 and is handled
+#' by \link{mvrsquared}[mvrsquared]
 #' @export
 #' @examples
 #' # Load a pre-formatted dtm and topic model
